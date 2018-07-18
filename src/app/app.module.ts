af4f1bf88348b8736b3069a1bb383f7d567550e1
Giampiero Bono <giampiero.bono@amadeus.com>
2018-07-18 16:39:35 +0200

    * Add ngrx and set it up * Add ngrx-store-freeze to be used in dev mode to avoid unpure state modifications * Add state logger to see the state logged in console only in dev mode * Add bootstrap css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
-import { AppComponent } from './app.component';
+import {environment} from '../environments/environment';
+import {AppComponent} from './app.component';
+import {metaReducers, reducers} from './reducers';
+import {StoreRouterConnectingModule, RouterStateSerializer} from '@ngrx/router-store';
+import {ROUTES} from './app.routes';
+import {CustomRouterStateSerializer} from './common/custom-router-serializer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    RouterModule.forRoot(ROUTES, {
+      useHash: Boolean(history.pushState) === false,
+      preloadingStrategy: PreloadAllModules
+    }),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+    }),
+    EffectsModule.forRoot([]),
   ],
-  providers: [],
+  providers: [
+    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
