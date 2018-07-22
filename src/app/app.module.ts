b8395b3d1a07d5104aed3102cf7adf1136cb5d35
Giampiero Bono <giampiero.bono@hotmail.it>
2018-07-22 15:51:37 +0200

    * General code reformat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import {NgModule} from '@angular/core';
-import {PreloadAllModules, RouterModule} from '@angular/router';
-import {BrowserModule} from '@angular/platform-browser';
-import {StoreModule} from '@ngrx/store';
-import {EffectsModule} from '@ngrx/effects';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import {environment} from '../environments/environment';
-import {AppComponent} from './app.component';
-import {metaReducers, reducers} from './reducers';
-import {StoreRouterConnectingModule, RouterStateSerializer} from '@ngrx/router-store';
-import {ROUTES} from './app.routes';
-import {CustomRouterStateSerializer} from './common/custom-router-serializer';
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { metaReducers, reducers } from './reducers';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
+import { ROUTES } from './app.routes';
+import { CustomRouterStateSerializer } from './common/custom-router-serializer';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import {CustomRouterStateSerializer} from './common/custom-router-serializer';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     RouterModule.forRoot(ROUTES, {
       useHash: Boolean(history.pushState) === false,
