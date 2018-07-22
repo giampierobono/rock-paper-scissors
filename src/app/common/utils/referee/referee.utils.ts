1abdf62c32a717469cb55d78d1cef106cf63d32c
Giampiero Bono <giampiero.bono@hotmail.it>
2018-07-22 15:49:51 +0200

    * Add referee utils

---
+++ src/app/common/utils/referee/referee.utils.ts
@@ -0,0 +1,19 @@
+import { GameAvailablePlay, GameResult } from '../../../models/index';
+
+export const learn = (rules: { [key: string]: { [key: string]: number } }) =>
+  (winner: GameAvailablePlay | string, loser: GameAvailablePlay | string): { [key: string]: { [key: string]: number } } => {
+    const clonedRules = { ...rules };
+    if (!clonedRules[winner]) {
+      clonedRules[winner] = {};
+    }
+    clonedRules[winner][loser] = 1;
+    return clonedRules;
+  };
+
+export const judge = (rules: { [key: string]: { [key: string]: number } }) =>
+  (play1: GameAvailablePlay | string, play2: GameAvailablePlay | string): GameResult => {
+    if (play1 === play2) {
+      return GameResult.TIE;
+    }
+    return rules[play1][play2] === 1 ? GameResult.PLAYER1_WIN : GameResult.PLAYER2_WIN;
+  };
